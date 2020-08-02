c527210a60613afd9145bd3a86fff1e49fdb4b77
Patrick Odermatt <odermatt-patrick@hotmail.com>
2020-08-02 20:18:57 +0200

    change load / show function

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,9 @@ $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps();
 });
 
-function loadNewMountain(nextMountainName) {
-    $("#mountainPlaceholder").fadeOut(500, function() {
-        nextMountainImage = "/images/" + nextMountainName + ".jpg"
-        nextImageMap = "#" + nextMountainName + "Map"
-        $("#mountainPlaceholder").attr("src", nextMountainImage);
-        $("#mountainPlaceholder").attr("usemap", nextImageMap);
-        console.log(nextImageMap)
-    }).fadeIn(500);
-  }
+function showNewMountain(currentMountainName, nextMountainName){
+    var currentImage = $("#" + currentMountainName + "Image")
+    var nextImage = $("#" + nextMountainName + "Image")
+    currentImage.css({display:'none'})
+    nextImage.css({display:'inline'})
+}
